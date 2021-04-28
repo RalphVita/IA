be34e44a56a7bc16c3f2397e87c8a7fc42d9a3b9
ralph_debian <ralph.vita@gmail.com>
2021-04-28 12:26:15 -0300

    analise

--- trab2/code/Analise.py
+++ trab2/code/Analise.py
@@ -4,6 +4,21 @@ from scipy.stats import zscore
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+nomes = [
+            'ZeroR', 
+            'Aleatório', 
+            'A. Est', 
+            'OneR', 
+            'NBayes G.', 
+
+            'Kmeans',
+            'KGA',
+            'Knn',
+            'DistKnn',
+            'Árv. D.',
+            'Florestas'
+        ]
+
 sns.set_theme()
 
 data_names = [
@@ -15,6 +30,7 @@ data_names = [
 
 for name in data_names:
     df = pd.read_csv(f'result/{name}_scores.csv')
+    #df['Classificador'] = nomes
     ax = sns.boxplot(x="Classificador", y='Acurácia', data=df)
-
+    ax.set_xticklabels(nomes)
 plt.show()
